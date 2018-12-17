bd922c6c8d7eb5674ff4d4796861c62c0d4794d7
Bruce Johnson <bruce@yogainternational.com>
2018-12-16 19:17:52 -0500

    completed problems 1 - 6

--- function.js
+++ function.js
@@ -1,1 +1,24 @@
 //your code here
+//1. Create a function called threeMinusTwo that returns the difference of 3 - 2
+
+
+//2. Create a function called returnString that returns a string
+
+
+
+//3. Create a function called returnVar that returns the returnThisVar variable
+var returnThisVar = 'Var returned!';
+
+
+//4. Create a function called addVars that adds the values of valueOne and valueTwo
+var valueOne = 5;
+var valueTwo = 5; 
+
+
+//5. Create a function called addNums that adds 34 and firstVal
+var firstVal = 10;
+
+
+//6. Create a function called subtractNums that subtracts 34 and firstVal
+
+
