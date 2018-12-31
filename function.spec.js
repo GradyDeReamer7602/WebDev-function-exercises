40d0398824f95029fa2e438fa0adab0079accf25
Bruce Johnson <bruce@yogainternational.com>
2018-12-31 15:47:41 -0500

    partially completed mocha and chai tests

---
+++ function.spec.js
@@ -0,0 +1,6 @@
+describe('threeMinusTwo',function(){
+   it('return the difference of 3 - 2 ', function(){
+      expect(threeMinusTwo()).to.equal(1);
+      expect(typeof threeMinusTwo()).to.equal('number');
+   })
+})
