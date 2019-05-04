c8bf0b2409fd019a031409ad4ff6a6487db6b807
GradyDeReamer7602 <50172539+GradyDeReamer7602@users.noreply.github.com>
2019-05-03 22:08:56 -0400

    It worked!

--- function.js
+++ function.js
@@ -1,24 +1,67 @@
 //your code here
 //1. Create a function called threeMinusTwo that returns the difference of 3 - 2
+var text = 3 - 2;
+
+//definition
+var threeMinusTwo = function() {
+	//function definition
+	return text;
+
+}
+//invocation
+threeMinusTwo();
 
 
 //2. Create a function called returnString that returns a string
 
 
+//definition
+var returnString = function() {
+	var returnString = ("string");
+	//function definition
+	return(returnString)
+}
+//invocation
+returnString();
 
 //3. Create a function called returnVar that returns the returnThisVar variable
-var returnThisVar = 'Var returned!';
+var returnVar = function(){
+	var returnVar = ("Var returned!");
+	return(returnVar)
+}
+returnVar();
+
 
 
 //4. Create a function called addVars that adds the values of valueOne and valueTwo
-var valueOne = 5;
-var valueTwo = 5; 
+var addVars = function(){
+		var valueOne = 5
+		var valueTwo = 5
+		var addValues = (valueOne + valueTwo)
+		return(addValues)
+}
 
+TotalValue();
 
 //5. Create a function called addNums that adds 34 and firstVal
-var firstVal = 10;
+function addNums(){
+	var firstVal = 10
+	var value = 34
+	var addValues = (firstVal + value)
+	return(addValues)
+}
 
-
+TotalValue();
 //6. Create a function called subtractNums that subtracts firstVal from 34
+function subtractNums(){
+	var firstVal = 10
+	var value = 34
+	var subtractValues = (value - firstVal)
+	return(subtractValues)
+}
+
+TotalValue()
+
+
 
 
